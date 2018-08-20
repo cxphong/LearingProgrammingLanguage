484c6ea91848dd63e36e26fd73f25d828b6d3541
caoxuanphong <caoxuanphong.khtn@gmail.com>
2018-08-20 12:24:23 +0700

    Update date-time.php

--- php/language/utils/date-time.php
+++ php/language/utils/date-time.php
@@ -1,11 +1,15 @@
 <?php
-	$date = date('Y-m-d H:i:s');
+
+	// Now
+	$datetime = new DateTime();
+	$datetime->format('Y-m-d H:i:s');
 
 	// Day diff 
 	$diff = date_diff($date1,$date2); //+272 days
-	$diff->days // positive
+	$diff->days // always positive
 	$diff->invert // 0: posstive, 1: negative
 
-	// Convert time format
+	// Convert string time to datetime
 	$token_end_date = date("d.m.Y", strtotime($device->token_end_date));
+
 ?>
